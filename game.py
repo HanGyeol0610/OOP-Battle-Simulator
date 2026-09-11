e4667270fb7823b81674dac761b6ceac405dce52
HanGyeol0610 <aagood0910@gmail.com>
2026-09-11 12:50:36 -0500

    Adding new goblin

--- game.py
+++ game.py
@@ -13,6 +13,11 @@ def main():
     goblin = Goblin("Griddle")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblin = Goblin("Scribble")
+    
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
